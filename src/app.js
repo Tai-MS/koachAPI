49fc938a8b4f4b6e1b3eb53cbfadce21ba70a38c
Hiro 010 <taiels94@gmail.com>
2024-06-22 18:43:30 -0300

    plantilla basica para express

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+//External imports
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
+//Internal imports
+import { __dirname, constants } from './utils/utils.js'
+import { connections, connectDB } from './utils/database.js'
 
+const app = express()
 
+//Middlewares
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser('myParser'))
 
-
+connections()
+app.listen(constants.PORT, () => {
+    console.log(`Server running at port ${constants.PORT}`);
+})
+connectDB()
 
 
