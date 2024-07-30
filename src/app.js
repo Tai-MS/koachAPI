09edb4e9302edf79043b46a1f25e2cb07267fe65
Hiro 010 <taiels94@gmail.com>
2024-07-30 16:42:36 -0300

    uploading API for koach

--- src/app.js
+++ src/app.js
@@ -1,22 +1,40 @@
-//External imports
-import express from 'express'
-import cookieParser from 'cookie-parser'
+// External imports
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUiExpress from 'swagger-ui-express';
+import path from 'path';
+// Internal imports
+import { __dirname, constants } from './utils/utils.js';
+import { connections, connectDB } from './utils/database.js';
+import userRouter from './router/user.router.js';
 
-//Internal imports
-import { __dirname, constants } from './utils/utils.js'
-import { connections, connectDB } from './utils/database.js'
+const app = express();
 
-const app = express()
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser('myParser'));
 
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(cookieParser('myParser'))
+app.use('/', userRouter);
 
-connections()
+// Swagger config
+const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: "User API.",
+            description: "User API documentation."
+        }
+    },
+    apis: [path.join(__dirname, '../documentation/*.yaml')]
+};
+const specs = swaggerJSDoc(swaggerOptions);
+console.log(path.join(__dirname, '../documentation/*.yaml'));
+app.use('/apidocs', swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
+
+connections();
 app.listen(constants.PORT, () => {
     console.log(`Server running at port ${constants.PORT}`);
-})
-connectDB()
-
-
+});
+connectDB();
